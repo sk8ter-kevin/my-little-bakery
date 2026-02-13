0a3b60b718bd79e939e34315ce25f9ecc308c621
sk8ter-kevin <kevinmeehan98@gmail.com>
2026-02-13 09:25:37 -0600

    Initial commit: baking calendar with photo uploads

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RecipeOrganizer from "../recipe-organizer.jsx";
+
+// Reset default browser styles
+document.documentElement.style.cssText = "height:100%;margin:0;padding:0;";
+document.body.style.cssText = "height:100%;margin:0;padding:0;font-family:system-ui,-apple-system,sans-serif;-webkit-font-smoothing:antialiased;";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RecipeOrganizer />
+  </React.StrictMode>
+);
